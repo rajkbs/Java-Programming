93ff91cb9e696d7070718b1e2d372a9b257aafa3
Raj Kumar Baliyar Singh <35659326+rajkbs@users.noreply.github.com>
2021-10-01 12:44:25 +0530

    Update ArrayProgram.java

--- ArrayProgram.java
+++ ArrayProgram.java
@@ -1,7 +1,43 @@
 public class ArrayProgram {
+    public static void main(String[] args) {
 
+        int a[];
+        ProgramOnArray poarr = new ProgramOnArray();
+        poarr.arrayDisplay();
+        poarr.arrayDataDisplay();
+
+        a = poarr.arrayReturn();
+        for (int r = 0; r < a.length; r++){
+            System.out.println(a[r]);
+        }
+    }
 }
 
 class ProgramOnArray{
 
+    int[] arr1;
+
+    int[] b = new int[]{5, 8, 9, 10, 3, 6};
+    int c[] = new int[]{10, 20, 30, 40};
+
+    public void arrayDisplay(){
+
+        for (int k : c) {               //foreach loop
+            System.out.println("Value is : " + k);
+        }
+    }
+
+    public void arrayDataDisplay(){
+
+        for (int i = 0; i < c.length; i++){
+            System.out.println("Value at " + i + " Location is " + c[i]);
+        }
+    }
+
+    //Returning an array data from a method
+    public int[] arrayReturn(){
+        int x[] = new int[]{5, 8, 9, 10, 3, 6};
+        return x;
+    }
 }
+
